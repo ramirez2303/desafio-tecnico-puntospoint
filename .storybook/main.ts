fdd9f4cea127365a8835f1e70a01fddc2580d187
Rodrigo Ramirez <rodrigoramirez@librodepases.com>
2025-03-05 02:13:27 -0300

    instalar y configurar de storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+    stories: [
+        "../stories/**/*.mdx",
+        "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    ],
+    addons: [
+        "@storybook/addon-essentials",
+        "@storybook/addon-onboarding",
+        "@chromatic-com/storybook",
+        "@storybook/experimental-addon-test",
+        "@storybook/addon-links",
+    ],
+    framework: {
+        name: "@storybook/nextjs",
+        options: {},
+    },
+    staticDirs: ["../public"],
+    core: {
+        builder: "webpack5",
+    },
+};
+export default config;
